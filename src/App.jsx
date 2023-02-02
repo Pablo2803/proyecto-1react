dc75c6baa2b20ac0840782bcc6ef98757628fc09
Pablo Arcienega <pabloarcienega1@gmail.com>
2023-02-01 23:19:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <ItemListContainer texto='Esteban' />
+    </>
+  );
+}
+
+export default App;
